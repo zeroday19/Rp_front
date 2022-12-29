e5b437ec5886ba659f045cb7e60c3578850e3120
unknown <themaker1602@gmail.com>
2022-12-29 16:55:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { ReactComponent as IconFacebook } from './assets/icons/facebook.svg';
+import { ReactComponent as IconTwitter } from './assets/icons/twitter.svg';
+import { ReactComponent as IconGithub } from './assets/icons/github.svg';
+import "./App.css";
+
+class App extends React.Component {
+  render = () => {
+    return (
+      <div className="card">
+        <div className="header">
+          <div className="logo">
+            <a href=".">RAREPAY</a>
+          </div>
+          
+        </div>
+        <div className="content">
+          <div className="title-holder">
+            <h1>Get ready for the change.</h1>
+            <p>Website coming soon. Please check back to know more. Shoot us an email if you're curious.</p>
+          </div>
+          <a href="mailto:asp.5616@gmail.com">
+            <div className="cta">Send us an email</div>
+          </a>
+        </div>
+        <div className="footer">
+          <span>made by <a className="underlined"  target="_blank" rel="noopener noreferrer">RarePay</a> using <a className="underlined" href="https://reactjs.org/" title="ReactJS" target="_blank" rel="noopener noreferrer">React</a></span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
